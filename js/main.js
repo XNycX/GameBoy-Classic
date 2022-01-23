f3e90304470729d508c47277b50fd68e86b01aac
XNycX <nycx2009@hotmail.com>
2022-01-23 19:25:19 +0100

    mejora del naming y añadido texto superior a la nintendo·

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const image = document.getElementById('image');
-const pantallaDiv = document.getElementById('btnPantalla')
+const btnPantalla = document.getElementById('btnPantalla')
 
 function playSonic() {
-    image.className += 'visible'
+    image.className = 'visible'
 }
 
-pantallaDiv.addEventListener('click', playSonic)
+btnPantalla.addEventListener('click', playSonic)
 
